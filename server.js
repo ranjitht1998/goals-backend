649c9c9779cc7beb10d5b3879e813967e8b4dde1
ranjitht1998 <ranjitht1998@gmail.com>
2022-05-16 10:02:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+//To accept sent in body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`server started on ${port}`));
